21ff129c412f806c419dc0b9f87697c149795e55
demirsubasic2001 <subasicd2000@gmail.com>
2023-05-20 19:25:51 +0200

    about-section-modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HeroSection from './components/hero-section/hero-section';
+import About from './components/about-section/about';
 
 function App() {
   return (
   <div>
     <HeroSection />
+    <About />
   </div>
   )
 }
---
+++ src/components/about-section/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './about.css'
+
+export default function About() {
+  return (
+    <div className='about-full-page'>
+        <div className="photo-side">
+        a
+        </div>
+        <div className="text-side">
+            <h1 className='about-title'>ABOUT ME</h1>
+            <p className='about-email'>subasicd2000@gmail.com</p>
+            <p className='about-description'>
+
+            </p>
+        </div>
+    </div>
+  )
+}
