2cb47fe3f01549b6f67d1f68145f6bd650e167a2
demirsubasic2001 <subasicd2000@gmail.com>
2023-05-27 21:00:13 +0200

    works-section

---
+++ src/components/images.js
@@ -0,0 +1,10 @@
+import slika1 from './image/slider-slike/slika1.jpg';
+import slika2 from './image/slider-slike/slika2.png';
+import slika3 from './image/slider-slike/slika3.jpg';
+import slika4 from './image/slider-slike/slika4.png';
+
+
+export default [{image: slika1, text: "Prvi Rad"},
+{image: slika2, text: "Drugi Rad"}, 
+{image: slika3, text: "Treci Rad"},
+{image: slika4, text: "Cetvrti Rad"}];
--- src/components/works-section/works.js
+++ src/components/works-section/works.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import {motion} from 'framer-motion';
+import { useRef, useEffect, useState } from 'react';
+import images from '../images'
 import './works.css'
 import Navbar from '../hero-section/navbar/navbar'
 
 export default function Works() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+
+  }, [])
+
+
   return (
     <div className='works-full-page'>
         <Navbar />
-      
-      works
+        <motion.div ref={carousel} className='carousel' whileTap={{cursor: "grabbing"}}>
+          <motion.div drag='x' dragConstraints={{right: 0, left: -width}} className='inner-carousel'>
+            {images.map(image => {
+              return (
+                <motion.div className='item' key={image}>
+                  <img src={image.image} alt="" />
+                  <a href="https://www.google.com/"><p className='slider-text'>{image.text}</p></a>
+                </motion.div>
+              )
+            })}
+
+          </motion.div>
+        </motion.div>
     </div>
   )
 }
